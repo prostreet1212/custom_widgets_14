327067b780074881f4d88e7681d4de714190718f
prostreet1212@gmail.com <ktndlf,tcfyu2020>
2022-12-15 23:34:30 +0300

    14

--- lib/widgets/weather_widget.dart
+++ lib/widgets/weather_widget.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
 
-
 class WeatherWidget extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    final painter = Paint()
-      ..color = Colors.blueAccent.withOpacity(0.5)
-        ..style=PaintingStyle.fill;
-    var path=Path()
-        ..moveTo(116, 10)
-    ..lineTo(172, 10)
-    ..lineTo(55, 126)
-    ..lineTo(28, 100)
-    ..close();
+     var paint3 = Paint()
+    ..color = Color.fromARGB(255, 250, 220, 92);
+    canvas.drawCircle(Offset(0.0, 0.0), 50, paint3);
+    Paint paint = Paint()..color = Colors.blue;
+    Paint paint2 = Paint()..color = Colors.green;
 
-    canvas.drawPath(path, painter);
+     double rectTop = 110.0;
+     double rectBottom = rectTop + 40.0;
+
+     double figureLeftEdge = size.width / 4;
+     double figureRightEdge = size.width - 90.0;
+     double figureCenter = size.width / 2;
+
+
+
+    RRect rect = RRect.fromRectAndRadius(
+        Rect.fromPoints(Offset(figureCenter, 130.0), Offset(110, 70)), Radius.circular(0));
+    canvas.drawRRect(rect, paint2);
+
+    canvas.drawCircle(Offset(5, 100.0), 30, paint);
+    canvas.drawCircle(
+      Offset(60, 90.0),
+      40.0,
+      paint,
+    );
+     canvas.drawCircle(
+      Offset(110, 97.0),
+      33.0,
+      paint,
+    );
+
+
   }
 
   @override
