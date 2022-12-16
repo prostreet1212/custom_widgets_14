d06f6c984a74b630994ab1f6d6ba7f2f29d8499a
prostreet1212 <prostreet1212@gmail.com>
2022-12-16 16:59:10 +0300

    weather

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,16 +1,37 @@
+import 'dart:math';
+
 import 'package:custom_widgets_14/widgets/weather_widget.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-   MyHomePage({Key? key}) : super(key: key);
-  double sliderValue=0;
+  MyHomePage({Key? key}) : super(key: key);
+  double sliderValue = 0;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  /*late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 2),
+    vsync: this,
+  );
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.fastOutSlowIn,
+  );*/
+
+  late final AnimationController controller = AnimationController(
+    duration: const Duration(seconds: 2),
+    vsync: this,
+  );
+  late final animation=Tween(begin: 0,end: 2*pi).animate(controller);
+
+
   @override
   Widget build(BuildContext context) {
+
+    double transform = 50;
 
     return Scaffold(
       appBar: AppBar(
@@ -18,31 +39,57 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         children: [
-              Align(
-                alignment: Alignment.topRight,
-                child: SizedBox(
-                  width: 150,
-                  height: 150,
-                  child: CustomPaint(
-                    painter: WeatherWidget(opacity: widget.sliderValue),
-                  ),
+          AnimatedBuilder(
+            animation: animation,
+           child: Align(
+              alignment: Alignment.topRight,
+              child: SizedBox(
+                width: 150,
+                height: 150,
+                child: CustomPaint(
+                  painter: WeatherWidget(opacity: widget.sliderValue),
                 ),
               ),
-          SizedBox(height: 30,),
+            ),
+            builder: (context, child) {
+              return Transform.rotate(
+                angle: animation.value.toDouble(),
+                child:Align(
+                  alignment: Alignment.topRight,
+                  child: SizedBox(
+                    width: 150,
+                    height: 150,
+                    child: CustomPaint(
+                      painter: WeatherWidget(opacity: widget.sliderValue),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+          SizedBox(
+            height: 30,
+          ),
           Slider(
-            min: 0,
+              min: 0,
               max: 1,
               divisions: 10,
               label: '${widget.sliderValue}',
               value: widget.sliderValue,
-              onChanged: (v){
+              onChanged: (v) {
                 setState(() {
-                  widget.sliderValue=v;
+                  widget.sliderValue = v;
                 });
               })
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            controller.forward();
+          });
+        },
+      ),
     );
   }
 }
-
