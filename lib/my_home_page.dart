25013ae08143376bfdcd9099a30eae79b8229bae
prostreet1212 <prostreet1212@gmail.com>
2022-12-16 16:05:58 +0300

    weather

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -2,8 +2,8 @@ import 'package:custom_widgets_14/widgets/weather_widget.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
+   MyHomePage({Key? key}) : super(key: key);
+  double sliderValue=0;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -11,16 +11,38 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Custom Widgets'),
-        ),
-        body: Container(
-          child: Center(
-            child: CustomPaint(
-              painter: WeatherWidget(),
-            ),
-          ),
-        ));
+      appBar: AppBar(
+        title: Text('Custom Widgets'),
+      ),
+      body: Column(
+        children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: SizedBox(
+                  width: 150,
+                  height: 150,
+                  child: CustomPaint(
+                    painter: WeatherWidget(opacity: widget.sliderValue),
+                  ),
+                ),
+              ),
+          SizedBox(height: 30,),
+          Slider(
+            min: 0,
+              max: 1,
+              divisions: 10,
+              label: '${widget.sliderValue}',
+              value: widget.sliderValue,
+              onChanged: (v){
+                setState(() {
+                  widget.sliderValue=v;
+                });
+              })
+        ],
+      ),
+    );
   }
 }
+
--- lib/widgets/weather_widget.dart
+++ lib/widgets/weather_widget.dart
@@ -1,44 +1,94 @@
 import 'package:flutter/material.dart';
 
 class WeatherWidget extends CustomPainter {
+  double opacity;
+
+  WeatherWidget({required this.opacity});
+
   @override
   void paint(Canvas canvas, Size size) {
-     var paint3 = Paint()
-    ..color = Color.fromARGB(255, 250, 220, 92);
-    canvas.drawCircle(Offset(0.0, 0.0), 60, paint3);
-    Paint paint = Paint()..color = Colors.blue;
-    Paint paint2 = Paint()..color = Colors.green;
+    Paint paint = Paint()
+      ..color = Colors.lightBlueAccent.withOpacity(getDropsOpacity(opacity));
+    Paint paint2 = Paint()
+      ..color = Colors.grey.withOpacity(getCloudOpacity(opacity));
+    var paint3 = Paint()
+      ..color =
+          Color.fromARGB(255, 250, 220, 92).withOpacity(getSunOpacity(opacity));
 
-     double rectTop = 110.0;
-     double rectBottom = rectTop + 40.0;
+    double x = 75;
+    double y = 75;
 
-     double figureLeftEdge = size.width / 4;
-     double figureRightEdge = size.width - 90.0;
-     double figureCenter = size.width / 2;
-
-
-
+    //Солнце
+    canvas.drawCircle(Offset(0.0 + x, 0 + y), 60, paint3);
+    //Облачко
     RRect rect = RRect.fromRectAndRadius(
-        Rect.fromPoints(Offset(figureCenter, 130.0), Offset(110, 70)), Radius.circular(0));
+        Rect.fromPoints(Offset(-55 + x, 40.0 + y), Offset(55 + x, 70 + y)),
+        Radius.circular(0));
     canvas.drawRRect(rect, paint2);
-
-    canvas.drawCircle(Offset(5, 100.0), 30, paint);
-
+    canvas.drawCircle(Offset(-55 + x, 40.0 + y), 30, paint2);
     canvas.drawCircle(
-      Offset(60, 90.0),
+      Offset(0 + x, 30.0 + y),
       40.0,
-      paint,
+      paint2,
     );
-
-     canvas.drawCircle(
-      Offset(110, 97.0),
+    canvas.drawCircle(
+      Offset(50 + x, 37 + y),
       33.0,
-      paint,
+      paint2,
     );
 
+    //Капли
+    Path path = Path()
+      ..moveTo(-55 + x, 80 + y)
+      ..lineTo(-49 + x, 82 + y)
+      ..lineTo(-59 + x, 102 + y)
+      ..lineTo(-65 + x, 100 + y)
+      ..close();
+    canvas.drawPath(path, paint);
 
+    Path path2 = Path()
+      ..moveTo(-5 + x, 80 + y)
+      ..lineTo(1 + x, 82 + y)
+      ..lineTo(-9 + x, 102 + y)
+      ..lineTo(-15 + x, 100 + y)
+      ..close();
+    canvas.drawPath(path2, paint);
+
+    Path path3 = Path()
+      ..moveTo(45 + x, 80 + y)
+      ..lineTo(51 + x, 82 + y)
+      ..lineTo(41 + x, 102 + y)
+      ..lineTo(35 + x, 100 + y)
+      ..close();
+    canvas.drawPath(path3, paint);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+
+  double getDropsOpacity(double value) {
+    if (value < 0.7) {
+      return 0;
+    }
+    return 10 / 3 * value - 7 / 3;
+  }
+
+  double getCloudOpacity(double value) {
+    /*  if(value>0.7){
+      return 1;
+    }
+    return (10/7*value);*/
+    if (value > 0.5) {
+      return 1;
+    }
+    return value * 2;
+    1 - (0.1 * 2);
+  }
+
+  double getSunOpacity(double value) {
+    if (value < 0.6) {
+      return 1;
+    }
+    return -2 * value + 2;
+  }
 }
