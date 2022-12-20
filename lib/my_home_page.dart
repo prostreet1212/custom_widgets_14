49beb5e5800c770412639beee2178150eeb00789
prostreet1212 <prostreet1212@gmail.com>
2022-12-20 16:40:06 +0300

    weather

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -21,29 +21,34 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   late final animation = Tween(begin: 1.0, end: 1.2).animate(controller);
   late final animation1 = Tween(begin: 0.5, end: 100.0).animate(controller);
   late final animation2 = Tween(begin: 0.0, end: 1.0).animate(controller);
-   Color selectedColor=Colors.red;
+  Color selectedColor = menuColorList[0].color;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Custom Widgets'),
-        actions: [
-          PopupMenuButton<MenuColor>(
-              icon: Icon(
-                Icons.square,
-                color: selectedColor,
+    return AppTheme(
+      appColor: selectedColor,
+      child: Builder(
+        builder: (innerContext) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(
+                'Custom Widgets',
+                style: TextStyle(color: AppTheme.of(innerContext)),
               ),
-            onSelected: (item){
-              setState(() {
-                if(selectedColor!=item.color){
-                  selectedColor=item.color;
-                  print(selectedColor.toString());
-                }
-              });
-            },
-              itemBuilder: (context) => //<PopupMenuEntry<MenuColor>>[
-                  menuColorList
+              actions: [
+                PopupMenuButton<MenuColor>(
+                  icon: Icon(
+                    Icons.square,
+                    color: selectedColor,
+                  ),
+                  onSelected: (item) {
+                    setState(() {
+                      if (selectedColor != item.color) {
+                        selectedColor = item.color;
+                      }
+                    });
+                  },
+                  itemBuilder: (context) => menuColorList
                       .map(
                         (colorItem) => PopupMenuItem<MenuColor>(
                           value: colorItem,
@@ -57,84 +62,78 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                         ),
                       )
                       .toList(),
-
-
-              //PopupMenuItem<MenuColor>(child: ListTile())
-              //]
-
-              )
-        ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          AnimatedBuilder(
-            animation: Listenable.merge([animation, animation1]),
-            builder: (context, child) {
-              return Transform(
-                transform: Matrix4.identity()
-                  ..translate(0.0, animation1.value)
-                  ..scale(animation.value),
-                transformHitTests: true,
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      if (controller.isCompleted) {
-                        controller.reverse();
-                      } else {
-                        controller.forward();
-                      }
-                    });
+                )
+              ],
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AnimatedBuilder(
+                  animation: Listenable.merge([animation, animation1]),
+                  builder: (context, child) {
+                    return Transform(
+                      transform: Matrix4.identity()
+                        ..translate(0.0, animation1.value)
+                        ..scale(animation.value),
+                      transformHitTests: true,
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            if (controller.isCompleted) {
+                              controller.reverse();
+                            } else {
+                              controller.forward();
+                            }
+                          });
+                        },
+                        child: SizedBox(
+                          height: 150 + animation1.value,
+                          width: 150,
+                          child: CustomPaint(
+                            painter: WeatherWidget(
+                                opacity: widget.sliderValue,
+                                animation: animation2),
+                          ),
+                        ),
+                      ),
+                    );
                   },
-                  child: SizedBox(
-                    height: 150 + animation1.value,
-                    width: 150,
-                    child: CustomPaint(
-                      painter: WeatherWidget(
-                          opacity: widget.sliderValue, animation: animation2),
-                    ),
-                  ),
                 ),
-              );
-            },
-          ),
 
-          //--------
-          Column(
-            children: [
-              Slider(
-                  min: 0,
-                  max: 1,
-                  divisions: 10,
-                  label: '${widget.sliderValue}',
-                  value: widget.sliderValue,
-                  onChanged: (v) {
-                    setState(() {
-                      widget.sliderValue = v;
-                    });
-                  }),
-              AppTheme(
-                  appColor: selectedColor,
-                  child: Builder(
-                    builder: (innerContext) {
-                      return InnerShadow(
-                          blur: 10,
-                          color: AppTheme.of(innerContext),
-                          offset: Offset(5, 5),
-                          child: Text(
-                            'Испортить погоду',
-                            style: TextStyle(
-                                fontSize: 44, fontWeight: FontWeight.bold),
-                          ));
-                    },
-                  )),
-              const SizedBox(
-                height: 20,
-                width: 100,
-              ),
-            ],
-          )
-        ],
+                //--------
+                Column(
+                  children: [
+                    Slider(
+                        min: 0,
+                        max: 1,
+                        divisions: 10,
+                        label: '${widget.sliderValue}',
+                        value: widget.sliderValue,
+                        onChanged: (v) {
+                          setState(() {
+                            widget.sliderValue = v;
+                          });
+                        }),
+                    InnerShadow(
+                      blur: 10,
+                      color: AppTheme.of(innerContext),
+                      offset: const Offset(5, 5),
+                      child: const Text(
+                        'Испортить погоду',
+                        style: TextStyle(
+                            fontSize: 44, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                      width: 100,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
